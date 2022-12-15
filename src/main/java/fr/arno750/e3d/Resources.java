8581390a09a3af656f57ed9da05d99df3f2d087c
Arnaud Wieland <awieland.us@gmail.com>
2022-12-15 13:47:00 +0100

    refactor resource

--- src/main/java/fr/arno750/e3d/Resources.java
+++ src/main/java/fr/arno750/e3d/Resources.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Enumeration;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -18,7 +17,7 @@ public class Resources {
     ResourceBundle resourceBundle;
 
     /**
-     * @param name
+     * @param name the resource name
      */
     public Resources(String name) {
         try {
@@ -31,209 +30,125 @@ public class Resources {
     }
 
     /**
-     * @param value
-     * @param size
-     * @return
-     */
-    public static String getHexString(int value, int size) {
-        String text = Integer.toHexString(value);
-        return "00000000".substring(8 - size + text.length()) + text;
-    }
-
-    /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return <tt>true</tt> is the key exits ; <tt>false</tt> otherwise.
      */
     public boolean containsKey(String key) {
         return resourceBundle.containsKey(key);
     }
 
     /**
-     * @return
-     */
-    public Enumeration<String> getKeys() {
-        return resourceBundle.getKeys();
-    }
-
-    /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>boolean</tt>.
      */
     public boolean getBoolean(String key) {
-        try {
-            return resourceBundle.getString(key).equals("true");
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return false;
+        return resourceBundle.getString(key).equals("true");
     }
 
     /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>integer</tt>.
      */
     public int getInteger(String key) {
-        try {
-            return Integer.parseInt(resourceBundle.getString(key));
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return -1;
+        return Integer.parseInt(resourceBundle.getString(key));
     }
 
     /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>double</tt>.
      */
     public double getDouble(String key) {
-        try {
-            return Double.parseDouble(resourceBundle.getString(key));
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return -1;
+        return Double.parseDouble(resourceBundle.getString(key));
     }
 
     /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>java.lang.String</tt>.
      */
     public String getString(String key) {
-        try {
-            return resourceBundle.getString(key);
-        } catch (Exception exception) {
-            System.err.println("Key [" + key + "] is missing in " + name);
-        }
-
-        return null;
+        return resourceBundle.getString(key);
     }
 
     /**
-     * The point should be defined as x,y
+     * The point is defined as <tt>x,y</tt>
      *
-     * @param key
-     * @return
+     * @param key the key.
+     * @return <tt>java.awt.Point</tt>.
      */
     public Point getPoint(String key) {
-        try {
-            String[] values = resourceBundle.getString(key).split(",");
+        String[] values = resourceBundle.getString(key).split(",");
 
-            int x = Integer.parseInt(values[0]);
-            int y = Integer.parseInt(values[1]);
-            return new Point(x, y);
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        int x = Integer.parseInt(values[0]);
+        int y = Integer.parseInt(values[1]);
+        return new Point(x, y);
     }
 
     /**
-     * The dimension should be defined as width,height
+     * The dimension is defined as <tt>width,height</tt>
      *
-     * @param key
-     * @return
+     * @param key the key.
+     * @return <tt>java.awt.Dimension</tt>.
      */
     public Dimension getDimension(String key) {
-        try {
-            String[] values = resourceBundle.getString(key).split(",");
+        String[] values = resourceBundle.getString(key).split(",");
 
-            int width = Integer.parseInt(values[0]);
-            int height = Integer.parseInt(values[1]);
-            return new Dimension(width, height);
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        int width = Integer.parseInt(values[0]);
+        int height = Integer.parseInt(values[1]);
+        return new Dimension(width, height);
     }
 
     /**
-     * The dimension should be defined as top,left,bottom,right
+     * The dimension is defined as <tt>top,left,bottom,right</tt>.
      *
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>java.awt.Insets</tt>.
      */
     public Insets getInsets(String key) {
-        try {
-            String[] values = resourceBundle.getString(key).split(",");
+        String[] values = resourceBundle.getString(key).split(",");
 
-            int top = Integer.parseInt(values[0]);
-            int left = Integer.parseInt(values[1]);
-            int bottom = Integer.parseInt(values[2]);
-            int right = Integer.parseInt(values[3]);
-            return new Insets(top, left, bottom, right);
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        int top = Integer.parseInt(values[0]);
+        int left = Integer.parseInt(values[1]);
+        int bottom = Integer.parseInt(values[2]);
+        int right = Integer.parseInt(values[3]);
+        return new Insets(top, left, bottom, right);
     }
 
     /**
-     * @param key
-     * @return
+     * The color is defined as <tt>#rrggbb</tt> with values in hexadecimal.
+     *
+     * @param key the key.
+     * @return the <tt>java.awt.Color</tt>.
      */
     public Color getColor(String key) {
-        try {
-            String colorName = resourceBundle.getString(key);
-            if (!colorName.startsWith("#"))
-                return null;
-            int colorValue = Integer.parseInt(colorName.substring(1), 16);
+        String colorName = resourceBundle.getString(key);
+        if (!colorName.startsWith("#"))
+            throw new IllegalArgumentException("Color format is #rrggbb with values in hexadecimal");
+        int colorValue = Integer.parseInt(colorName.substring(1), 16);
 
-            return new Color(colorValue);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        return new Color(colorValue);
     }
 
     /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>java.awt.Font</tt>.
      */
     public Font getFont(String key) {
-        try {
-            return Font.decode(resourceBundle.getString(key));
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        return Font.decode(resourceBundle.getString(key));
     }
 
     /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>javax.swing.ImageIcon</tt>.
      */
     public ImageIcon getImageIcon(String key) {
-        try {
-            return new ImageIcon(this.getClass().getResource(resourceBundle.getString(key)));
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        return new ImageIcon(this.getClass().getResource(resourceBundle.getString(key)));
     }
 
     /**
-     * @param key
-     * @return
+     * @param key the key.
+     * @return the <tt>javax.swing.KeyStroke</tt>.
      */
     public KeyStroke getKeyStroke(String key) {
-        try {
-            return KeyStroke.getKeyStroke(resourceBundle.getString(key));
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        return null;
+        return KeyStroke.getKeyStroke(resourceBundle.getString(key));
     }
 }
