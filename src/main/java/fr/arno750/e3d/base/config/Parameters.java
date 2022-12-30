0e08c26d2a62e1caa092e206c77b1baf62f52c6e
Arnaud Wieland <awieland.us@gmail.com>
2022-12-30 23:41:18 +0100

    use lombok

--- src/main/java/fr/arno750/e3d/base/config/Parameters.java
+++ src/main/java/fr/arno750/e3d/base/config/Parameters.java
@@ -1,40 +1,13 @@
 package fr.arno750.e3d.base.config;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Parameters {
     private int steps;
     private double ratio;
     private int latitudes;
     private int longitudes;
-
-    public int getSteps() {
-        return steps;
-    }
-
-    public void setSteps(int steps) {
-        this.steps = steps;
-    }
-
-    public double getRatio() {
-        return ratio;
-    }
-
-    public void setRatio(double ratio) {
-        this.ratio = ratio;
-    }
-
-    public int getLatitudes() {
-        return latitudes;
-    }
-
-    public void setLatitudes(int latitudes) {
-        this.latitudes = latitudes;
-    }
-
-    public int getLongitudes() {
-        return longitudes;
-    }
-
-    public void setLongitudes(int longitudes) {
-        this.longitudes = longitudes;
-    }
 }
--- src/main/java/fr/arno750/e3d/base/config/VolumeDefinition.java
+++ src/main/java/fr/arno750/e3d/base/config/VolumeDefinition.java
@@ -1,7 +1,11 @@
 package fr.arno750.e3d.base.config;
 
 import fr.arno750.e3d.base.Matrix;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class VolumeDefinition {
     private String name;
     private Status status;
@@ -12,45 +16,5 @@ public class VolumeDefinition {
 
     public Matrix getTransformMatrix() {
         return transform.getTransform();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Parameters getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(Parameters parameters) {
-        this.parameters = parameters;
-    }
-
-    public Transform getTransform() {
-        return transform;
-    }
-
-    public void setTransform(Transform transform) {
-        this.transform = transform;
     }
 }
