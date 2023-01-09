08bf8eb0464cfc6a4e5459a010283ec5a60dcc6e
Arnaud Wieland <awieland.us@gmail.com>
2023-01-09 22:55:10 +0100

    add unit test

--- src/main/java/fr/arno750/e3d/base/config/Parameters.java
+++ src/main/java/fr/arno750/e3d/base/config/Parameters.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 public class Parameters {
     private int steps;
-    private double ratio;
-    private int latitudes;
-    private int longitudes;
+    private double ratio = 0.5;
+    private int latitudes = 4;
+    private int longitudes = 4;
 }
--- src/test/java/fr/arno750/e3d/VolumeTest.java
+++ src/test/java/fr/arno750/e3d/VolumeTest.java
@@ -3,6 +3,7 @@ package fr.arno750.e3d;
 import fr.arno750.e3d.base.VertexFactory;
 import fr.arno750.e3d.base.config.VolumeDefinition;
 import fr.arno750.e3d.base.volume.Box;
+import fr.arno750.e3d.base.volume.Sphere;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,16 +11,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class VolumeTest {
 
     @Test
-    void whenWorkOutBoundingSphere() {
+    void givenBox_whenWorkOutBoundingSphere() {
         Box box = new Box(VolumeDefinition.getDefault(), new VertexFactory());
         box.workOutBoundingSphere();
         assertEquals(Math.sqrt(0.75), box.getRadius());
+        assertEquals(0.5, box.getCenter().x);
+        assertEquals(0.5, box.getCenter().y);
+        assertEquals(0.5, box.getCenter().z);
     }
 
     @Test
-    void whenWorkOutRitterBoundingSphere() {
+    void givenSphere_whenWorkOutBoundingSphere() {
+        var vd = VolumeDefinition.getDefault();
+        vd.getParameters().setLatitudes(32);
+        vd.getParameters().setLongitudes(64);
+        Sphere sphere = new Sphere(vd, new VertexFactory());
+        sphere.workOutBoundingSphere();
+        assertEquals(Math.sqrt(3), sphere.getRadius());
+    }
+
+    @Test
+    void givenBox_whenWorkOutRitterBoundingSphere() {
         Box box = new Box(VolumeDefinition.getDefault(), new VertexFactory());
         box.workOutRitterBoundingSphere();
         assertEquals(Math.sqrt(0.75), box.getRadius());
+        assertEquals(0.5, box.getCenter().x);
+        assertEquals(0.5, box.getCenter().y);
+        assertEquals(0.5, box.getCenter().z);
+    }
+
+    @Test
+    void givenSphere_whenWorkOutRitterBoundingSphere() {
+        var vd = VolumeDefinition.getDefault();
+        vd.getParameters().setLatitudes(32);
+        vd.getParameters().setLongitudes(64);
+        Sphere sphere = new Sphere(vd, new VertexFactory());
+        sphere.workOutRitterBoundingSphere();
+        assertEquals(1.0, sphere.getRadius());
     }
 }
